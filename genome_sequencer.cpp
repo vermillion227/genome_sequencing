bd949916a46a82dcf53b6c8c84019d8cbfe6813e
Patricia Tudor <ptudor@adobe.com>
2017-07-18 15:19:06 +0300

    add main skeleton

---
+++ genome_sequencer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#include "euler.h"
+
+using namespace std;
+
+int main(int argc, char* args[]) {
+    ifstream in("input.txt");
+    ofstream out("output.txt");
+
+    deBruijnGraph graph;
+
+    vector<string> comp;
+
+    return 1;
+}
